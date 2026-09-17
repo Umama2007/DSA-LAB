2687a80b1ad3ffb939662f5ea374d60733a9950d
Umama <umamasajid25@gmail.com>
2026-09-17 02:18:11 -0700

    tasks

--- LAB 1/task6.cpp
+++ LAB 1/task6.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 class Student {
 public:
     string name;
     int rollNo;
+
+    Student() {
+        name = "";
+        rollNo = 0;
+    }
+
+    void setData(string n, int r) {
+        name = n;
+        rollNo = r;
+    }
+
+    void display() {
+        cout << name << " - " << rollNo << endl;
+    }
+};
+
+int main() {
+    int n;
+
+    cout << "Enter number of students: ";
+    cin >> n;
+
+    Student* students = new Student[n];
+
+    for (int i = 0; i < n; i++) {
+        string name;
+        int roll;
+
+        cout << "Enter name and roll: ";
+        cin >> name >> roll;
+
+        students[i].setData(name, roll);
+    }
+
+    cout << "\nStudent List:\n";
+
+    for (int i = 0; i < n; i++) {
+        students[i].display();
+    }
+
+    delete[] students;
+
+    return 0;
+}
